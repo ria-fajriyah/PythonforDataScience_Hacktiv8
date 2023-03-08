cdadd988ca321da392c7175735be41b8c3c29a80
ria-fajriyah <riafajriyah14@gmail.com>
2023-03-08 12:47:01 +0700

    sesi3

---
+++ module/hello.py
@@ -0,0 +1,10 @@
+
+nama ="Nurul Fajriyah Al Maulidiyah"
+semester = 6
+prodi,fakultas = ["Teknik Informatika", "Ilmu Komputer"]
+
+def cetak(nama):
+    print(f"Nama: {nama}")
+
+class HelloPython:
+   pass
